6298aee790eab6896de6fe700957d016b292cfcc
Joshua Horvath <jsh_197@hotmail.com>
2025-09-16 18:15:51 -0400

    Removed redundant constants

--- FB_Versa3_Sense/stats-panel/common/constants.js
+++ FB_Versa3_Sense/stats-panel/common/constants.js
@@ -27,8 +27,3 @@ export const dataType = "cbor";
 export const dataFile = "weather.cbor";
 export const millisecondsPerMinute = 1000 * 60;
 export const wakeTime = 30 * millisecondsPerMinute;
-
-// constants for number modes
-export const hexadecimal = "Hexadecimal";
-export const standard_decimal = "Standard Decimal";
-export const full_hexadecimal = "Full Hexadecimal";
